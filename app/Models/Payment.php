981fb56c7c19fe68052f896bbc5aacf01eda9914
Akash Biswas <akash41bt@gmail.com>
2025-10-11 12:06:28 +0600

    restructure payment table

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -10,7 +10,7 @@ class Payment extends Model
     use HasFactory;
 
     protected $fillable = [
-        'student_id', 'amount', 'method', 'txnid', 'status'
+        'student_id', 'method', 'txnid', 'status'
     ];
 
     public function student()
